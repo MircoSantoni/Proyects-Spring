fc6fbe1f726da2017a726531ce863fb5d27d97bf
MircoSantoni <santonimircoariel@gmail.com>
2025-04-08 16:08:28 -0300

    .

--- springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/msvc_gatway/MsvcGatwayApplication.java
+++ springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/msvc_gatway/MsvcGatwayApplication.java
@@ -2,8 +2,10 @@ package com.mirco.springcloud.app.gateway.msvc_gatway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MsvcGatwayApplication {
 
 	public static void main(String[] args) {
