5eb12bdac0eedca1f49cec3f9c979b219fbc3516
MircoSantoni <santonimircoariel@gmail.com>
2025-04-08 15:40:11 -0300

    Gateway add

---
+++ msvc-gatway/src/main/java/com/mirco/springcloud/app/gateway/msvc_gatway/MsvcGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.mirco.springcloud.app.gateway.msvc_gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcGatwayApplication.class, args);
+	}
+
+}
