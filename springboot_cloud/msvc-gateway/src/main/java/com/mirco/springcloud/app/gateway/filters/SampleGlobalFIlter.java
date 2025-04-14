91dfd68b7bfda17b646aa42a71e0b172f98038b3
MircoSantoni <santonimircoariel@gmail.com>
2025-04-14 20:38:11 -0300

    SampleGlobalFilter

--- springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/filters/SampleGlobalFIlter.java
+++ springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/filters/SampleGlobalFIlter.java
@@ -20,58 +20,58 @@ public class SampleGlobalFIlter implements GlobalFilter, Ordered{
 
     private final Logger logger = LoggerFactory.getLogger(SampleGlobalFIlter.class);
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) { // <= Esto no funciona
-        logger.info("Ejecutando el filtro antes del request");
+    // @Override
+    // public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) { // <= Esto no funciona
+    //     logger.info("Ejecutando el filtro antes del request");
 
-        exchange.getRequest().mutate().headers(h -> h.add("Token", "Valor de token random")).build();
+    //     exchange.getRequest().mutate().headers(h -> h.add("Token", "Valor de token random")).build();
 
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            logger.info("Ejecutando el filtro despues del request");
+    //     return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+    //         logger.info("Ejecutando el filtro despues del request");
 
 
-            List<String> tokenList = exchange.getRequest().getHeaders().get("Token");
-            if (tokenList != null && !tokenList.isEmpty()) {
-                logger.info("Token: " + tokenList.get(0));
-            } else {
-                logger.warn("Token header no presente.");
-            }
+    //         List<String> tokenList = exchange.getRequest().getHeaders().get("Token");
+    //         if (tokenList != null && !tokenList.isEmpty()) {
+    //             logger.info("Token: " + tokenList.get(0));
+    //         } else {
+    //             logger.warn("Token header no presente.");
+    //         }
 
-            Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("Token")).ifPresent(value -> {
-                logger.info("Token: " + value);
-                exchange.getResponse().getHeaders().add("Token ", value);
-            });
+    //         Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("Token")).ifPresent(value -> {
+    //             logger.info("Token: " + value);
+    //             exchange.getResponse().getHeaders().add("Token ", value);
+    //         });
             
 
 
-            exchange.getResponse().getCookies().add("Color", ResponseCookie.from("Color", "Red").build());
-            exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
-        }));
-    }   
-        //     @Override
-        // public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //     logger.info("Ejecutando el filtro antes del request");
+    //         exchange.getResponse().getCookies().add("Color", ResponseCookie.from("Color", "Red").build());
+    //         exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+    //     }));
+    // }   
+            @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            logger.info("Ejecutando el filtro antes del request");
 
-        //     // Agregar header antes del request
-        //     ServerWebExchange mutatedExchange = exchange.mutate()
-        //         .request(exchange.getRequest().mutate()
-        //             .header("Token", "Valor de token random")
-        //             .build())
-        //         .build();
+            // Agregar header antes del request
+            ServerWebExchange mutatedExchange = exchange.mutate()
+                .request(exchange.getRequest().mutate()
+                    .header("Token", "Valor de token random")
+                    .build())
+                .build();
 
-        //     return chain.filter(mutatedExchange).then(Mono.fromRunnable(() -> {
-        //         logger.info("Ejecutando el filtro despues del request");
+            return chain.filter(mutatedExchange).then(Mono.fromRunnable(() -> {
+                logger.info("Ejecutando el filtro despues del request");
 
-        //         // Ahora podés loguear o setear headers en la response
-        //         Optional.ofNullable(mutatedExchange.getRequest().getHeaders().getFirst("Token")).ifPresent(value -> {
-        //             logger.info("Token: " + value);
-        //             mutatedExchange.getResponse().getHeaders().add("Token", value);
-        //         });
+                // Ahora podés loguear o setear headers en la response
+                Optional.ofNullable(mutatedExchange.getRequest().getHeaders().getFirst("Token")).ifPresent(value -> {
+                    logger.info("Token: " + value);
+                    mutatedExchange.getResponse().getHeaders().add("Token", value);
+                });
 
-        //         mutatedExchange.getResponse().getCookies().add("Color", ResponseCookie.from("Color", "Red").build());
-        //         mutatedExchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
-        //     }));
-        // }
+                mutatedExchange.getResponse().getCookies().add("Color", ResponseCookie.from("Color", "Red").build());
+                mutatedExchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+            }));
+        }
 
 
 
