17344314b8029392a23e5ec21fe3394c23bee243
MircoSantoni <santonimircoariel@gmail.com>
2025-04-15 18:22:32 -0300

    CookieGatewayFactory

--- springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/filters/factory/SampleCookieGatewayFIlterFactory.java
+++ springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/filters/factory/SampleCookieGatewayFIlterFactory.java
@@ -1,15 +1,25 @@
 package com.mirco.springcloud.app.gateway.filters.factory;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 import reactor.core.publisher.Mono;
 
 @Component
-public class SampleCookieGatewayFIlterFactory extends AbstractGatewayFilterFactory<SampleCookieGatewayFIlterFactory.ConfigurationCookie> {
+public class SampleCookieGatewayFilterFactory extends AbstractGatewayFilterFactory<SampleCookieGatewayFilterFactory.ConfigurationCookie> {
 
-    public SampleCookieGatewayFIlterFactory() {
+    
+    private final Logger logger = LoggerFactory.getLogger(SampleCookieGatewayFilterFactory.class);
+    public SampleCookieGatewayFilterFactory() {
         super(ConfigurationCookie.class);
     }
     
@@ -17,13 +27,32 @@ public class SampleCookieGatewayFIlterFactory extends AbstractGatewayFilterFacto
 
     @Override
     public GatewayFilter apply(ConfigurationCookie config) {
-        return (exchange, chain) -> {
+        return new OrderedGatewayFilter((exchange, chain) -> {
+            logger.info("Ejecutando pre gateway filter factory" + config.message);
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                Optional.ofNullable(config.value).ifPresent(cookie -> {
+                    exchange.getResponse().addCookie(ResponseCookie.from(config.name, config.value).build());
+                });
 
+                logger.info("Ejecutando post gateway filter factory" + config.message);
             }));
-        };
+        },100); // el ordered GateWayFilter es para ordenar la cadena de filtros
     }
+
+    @Override
+    public List<String> shortcutFieldOrder(){
+        return Arrays.asList("message", "name", "value");
+    }
+
+    
      
+    @Override
+    public String name() {
+        return "EjemploCookie";
+    }
+
+
+
     public static class ConfigurationCookie  {
         private String name;
         private String value;
