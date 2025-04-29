bf823db432fc1b7e273e8b1a132113b6f7e7caa3
MircoSantoni <santonimircoariel@gmail.com>
2025-04-29 19:00:40 -0300

    CRUD Product Entity

--- springboot_cloud/msvc-products/src/main/java/com/mirco/springcloud/msvc/products/repositories/ProductRepository.java
+++ springboot_cloud/msvc-products/src/main/java/com/mirco/springcloud/msvc/products/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.mirco.springcloud.msvc.products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mirco.springcloud.msvc.products.entitites.Product;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long>{
 
     
