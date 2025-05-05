a50ddbe186102197b8718e4ef95334a7d1970aa2
MircoSantoni <santonimircoariel@gmail.com>
2025-05-05 20:24:40 -0300

    Creacion del Microservicio Oauth

---
+++ springboot_cloud/msvc-oauth/src/test/java/com/mirco/springboot/msvc/oauth/msvc_oauth/MsvcOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirco.springboot.msvc.oauth.msvc_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
