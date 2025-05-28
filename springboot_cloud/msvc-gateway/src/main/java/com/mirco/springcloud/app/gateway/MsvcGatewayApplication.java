e5e1be1cbb5541ea177c17e8f5757ba00e7df391
MircoSantoni <santonimircoariel@gmail.com>
2025-05-28 20:13:43 -0300

    HotfixThis
    This needs more config

--- springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/MsvcGatewayApplication.java
+++ springboot_cloud/msvc-gateway/src/main/java/com/mirco/springcloud/app/gateway/MsvcGatewayApplication.java
@@ -2,12 +2,36 @@ package com.mirco.springcloud.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions.circuitBreaker;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.stripPrefix;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsvcGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcGatewayApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerConfig() {
+		return route("msvc-products")
+			.route(path("/api/products/**"), http())
+			.before(stripPrefix(2))
+			.filter(lb("msvc-products"))
+			.filter(circuitBreaker(config -> config
+			.setId("products")
+			.setStatusCodes("500")
+			.setFallbackPath("forward:/api/items/5")))
+			.before(stripPrefix(2)).build();
 	}
 
 }
